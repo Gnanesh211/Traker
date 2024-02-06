76f86e3bb39271413ffbbab85fe11b32b1fcd104
GNANESH P <karshvarth2112@gmail.com>
2024-02-06 14:58:21 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,6 +1,7 @@
 const express=require('express')
 const bodyparser=require('body-parser')
 const {connecttodb,getdb}=require('./db.cjs')
+const {ObjectId} =require('mongodb')
 const app = express()
 connecttodb((error)=>{
     if(error){
@@ -12,7 +13,7 @@ connecttodb((error)=>{
     console.log("running in  port 7000")
     }
 })
-app.use(bodyparser.json)
+app.use(bodyparser.json())
 
 app.get('/',((req,res) =>{
     res.send("Done...")
@@ -28,3 +29,59 @@ app.post('/detail',(req,res)=>{
         })
     })
 })
+app.get('/get', function(req, res) {
+    // Declaring an empty array
+    const entries = []
+    get.collection('Practice')
+    .find()////////////////for sort an array by individual is ------------>.find({"Amount" : -5000})
+    .forEach(entry => entries.push(entry))
+    .then(function() {
+        res.status(200).json(entries)
+    }).catch(function() {
+        res.status(500).json({
+            "status" : "Error Occured"
+        })
+    })
+})
+//////////////request.query.id----------->insterd of this use id num
+app.delete('/delete', function(request, response) {
+    if(ObjectId.isValid(request.query.id)) {
+        get.collection('Practice').deleteOne({
+            _id : new ObjectId(request.query.id)
+        }).then(function() {
+            response.status(200).json({
+                "status" : "Done"
+            })
+        }).catch(function() {
+            response.status(500).json({
+                "status" : "Error Occured"
+            })
+        })
+    } else {
+        response.status(500).json({
+            "status" : "ObjectId not valid"
+        })
+    }
+})
+///////request.params.id ----->insterd of this use id num
+app.patch('/update/:id', function(request, response) {
+    if(ObjectId.isValid(request.params.id)) {
+        get.collection('Practice').updateOne(
+            { _id : new ObjectId(request.params.id) }, // identifier : selecting the document which we are going to update
+            { $set : request.body } // The data to be updated
+        ).then(function() {
+            response.status(200).json({
+                "status" : "Done"
+            })
+        }).catch(function() {
+            response.status(500).json({
+                "status" : "Error Occured"
+            })
+        })
+    } else {
+        response.status(500).json({
+            "status" : "ObjectId not valid"
+        })
+    }
+})
+
