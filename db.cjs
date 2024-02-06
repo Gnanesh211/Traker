f54cfc19d096867b3e52a6adca532ab6768907d2
GNANESH P <karshvarth2112@gmail.com>
2024-02-06 14:57:37 +0530

    Update db.cjs

--- db.cjs
+++ db.cjs
@@ -1,7 +1,7 @@
 const{MongoClient}=require("mongodb")
 let db
 function connecttodb(call){
-    db=MongoClient.connect('mongodb://localhost:27017/DivineDB').then((client)=>{
+    db=MongoClient.connect('mongodb+srv://Gnanesh:SEN211gks@cluster0.mt9rhz2.mongodb.net/Tracker?retryWrites=true&w=majority').then((client)=>{
         db=client.db()
         call()
     }).catch((error)=>{
